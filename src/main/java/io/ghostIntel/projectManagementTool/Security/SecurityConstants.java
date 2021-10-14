f68ab8adf4422143d634a8494d58a736ccaad28c
akshay117 <akshayvanil117@gmail.com>
2021-10-14 22:20:53 +0530

    added Procfile

--- src/main/java/io/ghostIntel/projectManagementTool/Security/SecurityConstants.java
+++ src/main/java/io/ghostIntel/projectManagementTool/Security/SecurityConstants.java
@@ -9,5 +9,5 @@ public class SecurityConstants {
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300_000; //30 seconds
+    public static final long EXPIRATION_TIME = 3000000; //30 seconds
 }
