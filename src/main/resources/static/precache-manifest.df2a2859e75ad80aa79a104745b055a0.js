525fa2ad2d5c2572054dc97025c2e6801532e94e
Akshay V Anil <akshayvanil117@gmail.com>
2022-05-23 00:33:05 +0530

    Added ReactFrontend

---
+++ src/main/resources/static/precache-manifest.df2a2859e75ad80aa79a104745b055a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565a30a702acf179fac7f992b8203d85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5720896cac7ea2bef5e0",
+    "url": "/static/css/main.5d560caf.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "5720896cac7ea2bef5e0",
+    "url": "/static/js/main.c13605d0.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
